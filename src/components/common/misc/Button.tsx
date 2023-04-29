d2130d7e4eb3440fc12e2410282e5bacbedd1495
Edmilson-C <edmilson.conceicao09@gmail.com>
2023-04-29 18:22:29 +0200

    refactor(button): :lipstick: show ::after properties if button is not loading or disabled

--- src/components/common/misc/Button.tsx
+++ src/components/common/misc/Button.tsx
@@ -34,7 +34,7 @@ const Button = ({
     if (loading || disabled) {
       setClassList(`${classList} opacity-50 cursor-not-allowed`)
     } else {
-      setClassList(`${classList} cursor-pointer`)
+      setClassList(`${classList} cursor-pointer after:content-[''] after:inline-block after:absolute after:top-0 after:left-0 after:w-full after:h-full after:opacity-0 after:transition-all after:duration-800 after:active:opacity-100`)
     }
   }, [])
 
@@ -43,7 +43,7 @@ const Button = ({
       type={type}
       disabled={disabled}
       onClick={clickedEvent}
-      className={`${classList} bg-cyan-400 hover:bg-cyan-500 after:bg-cyan-300 text-white relative rounded py-2 px-5 text-sm capitalize shadow after:content-[''] after:inline-block after:absolute after:top-0 after:left-0 after:w-full after:h-full after:opacity-0 after:transition-all after:duration-800 after:active:opacity-100`}
+      className={`${classList} bg-cyan-400 hover:bg-cyan-500 after:bg-cyan-300 text-white relative rounded py-2 px-5 text-sm capitalize shadow`}
     >
       {loading ? (
         <Loading />
