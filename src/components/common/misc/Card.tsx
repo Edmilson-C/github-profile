4627db30e6b84e3b680d47305f5384b3a8cdb227
Edmilson-C <edmilson.conceicao09@gmail.com>
2024-01-27 16:10:35 +0200

    feat: :sparkles: create card component

--- src/components/common/misc/Card.tsx
+++ src/components/common/misc/Card.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+interface CardProps {
+  label: string
+  description: string | number
+}
 
-const Card = ({ label, description }) => (
-  <div>Card</div>
+const Card = ({ label, description }: CardProps) => (
+  <div className="flex items-center justify-between rounded-xl bg-blue-dark py-2 px-6 h-14">
+    <span className="text-gray-light text-base">{label}</span>
+    <div className="h-full w-px bg-light-default opacity-50 mx-6" />
+    <span className="text-light-default text-base">{description}</span>
+  </div>
 )
 
 export default Card
