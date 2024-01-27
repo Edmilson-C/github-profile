d76e11f29e20e2f7d40961d27dcff40b39b08273
Edmilson-C <edmilson.conceicao09@gmail.com>
2024-01-27 15:22:08 +0200

    chore: :wrench: configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          light: '#3662E3',
+          regular: '#1D1B48',
+          default: '#111729'
+        },
+        gray: {
+          light: '#4A5567',
+          regular: '#364153',
+          dark: '#20293A'
+        },
+        light: {
+          default: '#CDD5E0'
+        },
+        dark: {
+          default: '#0F0F0F'
+        }
+      },
+      fontFamily: {
+        sans: ['Be Vietnam Pro', ...defaultTheme.fontFamily.sans]
+      },
+      fontSize: {
+        large: '2rem'
+      }
+    }
   },
   plugins: []
 }
