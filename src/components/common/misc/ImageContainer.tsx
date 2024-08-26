bed75aa430595a24a98c97079f272dc87ba7fa59
Edmilson-C <edmilson.conceicao09@gmail.com>
2024-08-26 22:10:08 +0200

    style(image container): :lipstick: update width and border

--- src/components/common/misc/ImageContainer.tsx
+++ src/components/common/misc/ImageContainer.tsx
@@ -5,8 +5,8 @@ interface ImageContainerProps {
 }
 
 const ImageContainer = ({ image, name, className }: ImageContainerProps) => (
-  <div className={`${className} w-40 h-40 border-gray-dark border-8 rounded-2xl`}>
-    <img className="rounded-lg w-full h-full" src={image} alt={`${name} profile pic`} />
+  <div className={`${className} w-36 h-36 border-gray-dark border-8 rounded-3xl`}>
+    <img className="rounded-2xl w-full h-full" src={image} alt={`${name} profile pic`} />
   </div>
 )
 
