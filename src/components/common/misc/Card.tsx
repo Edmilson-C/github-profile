96375ee20757e2c1d0a3e5cbbfe5c6518af13905
Edmilson-C <edmilson.conceicao09@gmail.com>
2024-08-26 22:11:21 +0200

    feat(card component): :sparkles: add className as props

--- src/components/common/misc/Card.tsx
+++ src/components/common/misc/Card.tsx
@@ -1,10 +1,11 @@
 interface CardProps {
   label: string
   description: string | number
+  className?: string
 }
 
-const Card = ({ label, description }: CardProps) => (
-  <div className="flex items-center justify-between rounded-xl bg-blue-dark py-2 px-6 h-14">
+const Card = ({ label, description, className }: CardProps) => (
+  <div className={`${className} flex items-center justify-between rounded-xl bg-blue-dark py-2 px-6 h-14`}>
     <span className="text-gray-light text-base">{label}</span>
     <div className="h-full w-px bg-light-default opacity-50 mx-6" />
     <span className="text-light-default text-base">{description}</span>
